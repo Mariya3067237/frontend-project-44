e0cbb96fda201f60c2e4583b3b16bf3bbc8389d8
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-25 20:23:21 +0300

    add src/even-game.js

--- src/even-game.js
+++ src/even-game.js
@@ -3,18 +3,19 @@ import { hello } from '../src/cli.js';
 import readlineSync from 'readline-sync';
 
 
-const checkParity = (checkNumber) => {
-    if (checkNumber % 2 === 0) {
-        return 'yes';
-    }
-    return 'no';
-}
+
+
 export const evenGame = () => {
     const name = hello();
     console.log('Answer "yes" if the number is even, otherwise answer "no".')
     
     
     for (let i = 0; i < 3; i += 1) {
+        if (i === 3){
+            console.log(`Congratulations, ${name}!`);
+            return;
+        }
+        const checkParity = randomNumber % 2 === 0 ? 'yes' : 'no';
         const randomNumber = getRandomNumber();
         const result = checkParity(randomNumber);
         console.log(`Question: ${randomNumber}`);
@@ -22,8 +23,6 @@ export const evenGame = () => {
         if (answer.toLowerCase !== result) {
             console.log(`'${answer.toLowerCase()}' is wrong answer ;(. Correct answer was '${result}'\nLet's try again, ${name}!`); 
             break;
-        } else if (i === 3){
-            console.log(`Congratulations, ${name}!`);
         } else {
             console.log('Correct!');
         }
