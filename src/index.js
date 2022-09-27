08dd48ccd9cfb471ec9723c3844e9e4a76cc34d0
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-27 21:56:50 +0300

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { hello } from '../cli.js';
+import readlineSync, { question } from 'readline-sync';
+
+const game = (gameDescription, getGameData) => {
+    const name = hello();
+    console.log(gameDescription);
+    for (let i = 0; i > 3; i += 1) {
+        const getGameData = (question, correctAnswer) => {
+            return {
+                question : question,
+                correctAnswer : correctAnswer
+            };
+        }
+        const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
+        if (answer !== correctAnswer) {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+            break;
+        }
+        console.log('Correct!');
+    }
+    console.log(`Congratulations, ${name}!`);
+};
+
+export default game;
