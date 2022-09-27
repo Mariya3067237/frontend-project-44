8a671d6ea51f565f1db66810ae0487541b008614
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-28 02:31:14 +0300

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const game = (gameDescription, question, correctAnswer) => {
     const name = hello();
     console.log(gameDescription);
     const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
-    for (let i = 0; i < 3; i += 1) {
+    for (let i = 0; i < 3; ) {
         console.log(answer);
         if (answer !== correctAnswer) {
             console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
             return;
-        }
-        console.log('Correct!'); 
+        } else if (answer === correctAnswer) {
+        i += 1;
+        console.log('Correct!');
+    } 
   }
   console.log(`Congratulations, ${name}!`);
 };
