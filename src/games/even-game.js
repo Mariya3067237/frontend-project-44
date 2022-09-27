bf9d6e3b8548ca90170b3854017b0188e8e5b963
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-27 21:56:28 +0300

    add src/games/even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,28 +1,19 @@
 import { getRandomNumber } from '../cli.js';
-import { hello } from '../cli.js';
-import readlineSync from 'readline-sync';
+import readlineSync, { question } from 'readline-sync';
+import gameRun from '../index.js';
 
-export const evenGame = () => {
-    const name = hello();
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    for (let i = 0; i < 3; i += 1) {
-        if (i === 3) {
-            console.log(`Congratulations, ${name}!`);
-            return;
-        }
-        const randomNumber = getRandomNumber();
-        const checkParity = randomNumber % 2 === 0 ? 'yes' : 'no';
-        const result = checkParity;
-        console.log(`Question: ${randomNumber}`);
-        const answer = readlineSync.question('Your answer: ');
-        if (answer.toLowerCase === result) {
-            console.log('Correct!');
-        } else {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'\nLet's try again, ${name}!`); 
-            break;
-        }
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+const checkParity = randomNumber % 2 === 0 ? 'yes' : 'no';
 
+const getGameData = () => {
+    const randomNumber = getRandomNumber();
+    const question = randomNumber;
+    const correctAnswer = checkParity();
+    return {
+        question : question,
+        correctAnswer : correctAnswer
     }
-    
 };
+
+export default () => gameRun(getGameData, gameDescription);
 
