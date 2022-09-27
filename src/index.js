0b5b0ab56211fece2a77f0f81f07f72f0cc58e7e
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-27 22:08:02 +0300

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ const game = (gameDescription, getGameData) => {
     const name = hello();
     console.log(gameDescription);
     for (let i = 0; i > 3; i += 1) {
-        const getGameData = () => {
-            return [question, correctAnswer];
-        }
+        const [question, correctAnswer] = getGameData();
         const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
         if (answer !== correctAnswer) {
             console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
