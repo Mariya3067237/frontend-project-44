440166ac61879c9bcfb06cc86c273226bff255a3
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-28 03:28:37 +0300

    add src/games/even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,8 +1,8 @@
 import { getRandomNumber } from '../cli.js';
 import game from '../index.js';
 
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 const gameData = () => {
-    const description = 'Answer "yes" if the number is even, otherwise answer "no".';
     let randomNumber = getRandomNumber();
     let checkParity = randomNumber % 2 === 0 ? 'yes' : 'no';
     let question = randomNumber;
