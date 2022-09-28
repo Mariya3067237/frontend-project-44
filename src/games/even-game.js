92d680606e0991663ac894f1548e9129de78d8e4
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-28 03:15:12 +0300

    add src/games/even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -2,12 +2,12 @@ import { getRandomNumber } from '../cli.js';
 import game from '../index.js';
 
 export const evenGame = () => {
-    const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+    const description = 'Answer "yes" if the number is even, otherwise answer "no".';
     let randomNumber = getRandomNumber();
     let checkParity = randomNumber % 2 === 0 ? 'yes' : 'no';
     let question = randomNumber;
     let correctAnswer = checkParity;
-    game(gameDescription, question, correctAnswer);
+    return [question, correctAnswer];
 };
 
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { hello } from './cli.js';
 import readlineSync from 'readline-sync';
 
-const game = (gameDescription, question, correctAnswer) => {
+const game = (description, gameData) => {
     const name = hello();
-    console.log(gameDescription);
+    console.log(description);
     for (let i = 0; i < 3; i += 1) {
+        const [question, correctAnswer] = gameData();
         let answer = readlineSync.question(`Question: ${question}\nYour answer: `);
         if (answer !== correctAnswer) {
             console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
