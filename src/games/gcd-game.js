605dfa81a7eb6bbce1b00dc0ba3d7f582ec83bd4
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-29 22:46:08 +0300

    add src/games/gcd-game.js

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -2,7 +2,7 @@ import { getRandomNumber } from '../cli.js';
 import game from '../index.js';
 
 const descriprion = 'Find the greatest common divisor of given numbers.';
-const getGreatComonDivider = (num1, num2) => {
+const getGreatCommonDivider = (num1, num2) => {
   const maxNum = Math.max(num1, num2);
   const minNum = Math.min(num1, num2);
   let divider = minNum;
@@ -18,8 +18,8 @@ const round = () => {
   const num1 = getRandomNumber();
   const num2 = getRandomNumber();
   const question = `${num1} ${num2}`;
-  const correctAnswer = getGreatComonDivider(number1, number2);
-  return [question, String(correctAnswer)];
+  const correctAnswer = getGreatCommonDivider(num1, num2);
+  return [question, correctAnswer];
 };
 
 export default () => game(descriprion, round);
