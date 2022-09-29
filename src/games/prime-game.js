2a27489bcc148e90b9047a224ca785ecc0dc6121
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-30 00:30:19 +0300

    add src/games/prime-game.js

---
+++ src/games/prime-game.js
@@ -0,0 +1,25 @@
+import { getRandomNumber } from '../cli.js';
+import game from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const round = () => {
+  let num = getRandomNumber();
+  const question = num;
+  const correctAnswer = isPrime(num) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default () => game(description, round);
