74373f396e4973b050321fe136968c51c2637dab
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-30 02:10:58 +0300

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import { hello } from './cli.js';
-import readlineSync from 'readline-sync';
+import { hello } from "./cli.js";
+import readlineSync from "readline-sync";
 
 const game = (description, round) => {
-    const name = hello();
-    console.log(description);
-    for (let i = 0; i < 3; i += 1) {
-        const [question, correctAnswer] = round();
-        let answer = readlineSync.question(`Question: ${question}\nYour answer: `);
-        answer = answer.toLowerCase();
-        if (answer !== correctAnswer) {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-            return;
-        }
-        console.log('Correct!');
+  const name = hello();
+  console.log(description);
+  for (let i = 0; i < 3; i += 1) {
+    const [question, correctAnswer] = round();
+    let answer = readlineSync.question(`Question: ${question}\nYour answer: `);
+    answer = answer.toLowerCase();
+    if (answer !== correctAnswer) {
+      console.log(
+        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`
+      );
+      return;
     }
-    console.log(`Congratulations, ${name}!`);
+    console.log("Correct!");
+  }
+  console.log(`Congratulations, ${name}!`);
 };
 
 export default game;
