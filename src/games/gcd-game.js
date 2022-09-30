dbd87dba878b50b165ac002bf9c7db3ddb004d27
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-30 17:35:19 +0300

    add src/games/gcd-game.js

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,14 +1,14 @@
-import { getRandomNumber } from '../get-random-num.js';
+import getRandomNumber from '../get-random-num.js';
 import game from '../index.js';
 
 const descriprion = 'Find the greatest common divisor of given numbers.';
 const getGreatCommonDivider = (num1, num2) => {
   if (num2) {
-      return getGreatCommonDivider(num2, num1 % num2);
+    return getGreatCommonDivider(num2, num1 % num2);
   } else {
-      return Math.abs(num1);
+    return Math.abs(num1);
   }
-}
+};
 
 const getRoundData = () => {
   const numberOne = getRandomNumber();
