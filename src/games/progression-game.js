8bce337ae541f36ea58f3fc375e85b0eeb4d685c
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-30 17:36:48 +0300

    add src/games/progression-game.js

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -1,4 +1,4 @@
-import { getRandomNumber } from '../get-random-num.js';
+import getRandomNumber from '../get-random-num.js';
 import game from '../index.js';
 
 const description = 'What number is missing in the progression?';
@@ -17,7 +17,7 @@ const getRoundData = () => {
   const progressionStep = getRandomNumber(1, 10);
   const lengthProgression = getRandomNumber(5, 10);
   const progressionRound = getProgression(startNum, progressionStep, lengthProgression);
-  const emptyIndex = getRandomNumber(1, progressionRound.length) - 1
+  const emptyIndex = getRandomNumber(1, progressionRound.length) - 1;
   const correctAnswer = String(progressionRound[emptyIndex]);
   progressionRound[emptyIndex] = '..';
   const question = `${progressionRound.join(' ')}`;
