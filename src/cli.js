fb1ba7c5106e8f5d070b121873acdb35bfc4d124
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-30 17:15:02 +0300

    add src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,7 @@
-/* eslint-disable max-len */
 import readlineSync from 'readline-sync';
 
 export const hello = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
-  return name;
 };
-
-export const getRandomNumber = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
