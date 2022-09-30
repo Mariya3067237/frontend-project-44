b4f12b0aa4bf0f35f51d2d58a21662b88000fdd3
Mariya Malandina <mariyamalandina@gmail.com>
2022-09-30 17:37:10 +0300

    add src/games/prime-game.js

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,4 +1,4 @@
-import { getRandomNumber } from '../get-random-num.js';
+import getRandomNumber from '../get-random-num.js';
 import game from '../index.js';
 
 const description = 'Answer \'yes\' if given number is prime. Otherwise answer \'no\'.';
