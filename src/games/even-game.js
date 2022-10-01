572ec0654aa4dac92de8d4fe24bbefef58520a19
Mariya Malandina <mariyamalandina@gmail.com>
2022-10-01 19:52:47 +0300

    add src/games/even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -4,9 +4,8 @@ import engineGame from '../index.js';
 const description = 'Answer \'yes\' if number even otherwise answer \'no\'.';
 const isEven = (checkNumber) => checkNumber % 2 === 0;
 const getRoundData = () => {
-  const randomNumber = getRandomNumber();
   const question = getRandomNumber();
-  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
